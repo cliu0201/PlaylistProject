bcb2df018cc31fa916df2ae930f25c8d379bee82
cliu0201 <cliu26@students.polytechnic.org>
2026-01-23 19:01:18 -0800

    Cici's commit to PlaylistTester.java

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -3,53 +3,52 @@
  * playlist in the main method. You don't need to follow the testing specifications of this exactly
  * if you want to write your own separate tester instead.
  * Note that there's no need for a Scanner in this project-- all of the playlist can be 'hardcoded' into main.
- * @author
- * @version
+ * @author Cici Liu & Hera Yuan
+ * @version Cici's commmit
  */
 public class PlaylistTester {
     public static void main(String[] args) {
         System.out.println("Initializing a Playlist...\n");
-        //Make your playlist here
+        Playlist p = new Playlist();
 
         System.out.println("Adding songs to the Playlist...\n");
-        /**
-         * Add some songs here. Note that the format for adding a Song to a Playlist p
-         * is something like...
-         * p.addSong(new Song(..., ..., ...))
-         */
-
-
-
+        Song s1 = new Song("Everything Everywhere", "Noah Kahan", 257.0 / 60.0);
+        Song s2 = new Song("OK Love You Bye", "Olivia Dean", 154.0 / 60.0);
+        Song s3 = new Song("Tears", "Sabrina Carpenter", 160.0 / 60.0);
+        Song s4 = new Song("Free Now", "Gracie Abrams", 214.0 / 60.0);
+        p.addSong(s1);
+        p.addSong(s2);
+        p.addSong(s3);
+        p.addSong(s4);
+        
         System.out.println("Printing the songs...\n");
         //Print out all the songs in the playlist to verify it's working correctly
+        p.listSongs();
 
-
-        System.out.println("\nLiking the songs in position X, Y, Z, etc....\n");
-        //Once your songs are 'liked', this should be reflected in the next printout
-
-
-        System.out.println("Printing the songs...\n");
-
-
-        System.out.println("\nRemoving the song in position A, B, C, etc...\n");
-
+        System.out.println("\nLiking the songs 'Everything Everywhere' and 'Tears'\n");
+        p.likeSong(s1);
+        p.likeSong(s3);
 
         System.out.println("Printing the songs...\n");
+        p.listSongs();
 
+        System.out.println("\nRemoving the song 'OK Love You Bye'\n");
+        p.removeSong(s2);
+
+        System.out.println("Printing the songs...\n");
+        p.listSongs();
 
         System.out.println("\nPrinting only the liked songs...\n");
-        //Your Playlist should be able to do this without looping while in main!
-
+        p.listLikedSongs();
 
         System.out.println("\nPrinting the total duration of all songs...\n");
-        //Note that the format should look something like minutes:seconds
-
+        System.out.println(p.getPlaylistDuration());
 
         System.out.println("\nRemoving all unliked songs from the playlist...\n");
-        //This should be doable with a single method call
-
+        p.removeUnlikedSongs();
 
         System.out.println("Printing all songs...\n");
-        //This should now look like only the liked songs list from before
+        p.listSongs();
+        System.out.println();
     }
 }
